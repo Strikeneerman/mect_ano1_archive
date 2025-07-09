35ff6251f9a4ef36bcac1c7755cc60babd6e27d5
Leandro Rito <48207798+Strikeneerman@users.noreply.github.com>
2025-07-09 10:29:46 +0100

    added IC

---
+++ Semestre1/IC/Pratico/P/P/Project #01/texto.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Numbers {
+
+    private:
+
+     int a;
+     int b;
+
+
+
+
+};
+
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
